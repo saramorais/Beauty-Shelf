8717518d84642ad3848a03b76b5704dbed6b79b9
Sara Morais <saracristinasm@gmail.com>
2018-09-11 19:59:57 -0400

    view change for heroku

--- app.js
+++ app.js
@@ -10,17 +10,21 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
+/* view engine setup */
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+/* uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))); */
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+/*REACT FOLDER */
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static('client/public'));
 
 
 app.use(function(req, res, next) {
